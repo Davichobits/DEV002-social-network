da43c6b39462a8e1e1e3480c4be83a5b83af1879
David <gms2programador@gmail.com>
2023-01-20 10:38:39 -0500

    configuración inicial firebase

---
+++ src/firebase/initFirebase.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-unresolved */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js';
+import { firebaseConfig } from './firebaseConfig.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
