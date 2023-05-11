496fd1512d74ca1c84e729b299cf9f60964e436b
Davichobits <davidruizdeveloper@gmail.com>
2023-05-11 06:02:01 -0500

    agregado de archivo de configuracion

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const config = {
+  apiKey: 'AIzaSyDnpwSefyJJ0ReocETac1LLPLfA0VthZ3A',
+  authDomain: 'social-network-laborator-8e757.firebaseapp.com',
+  projectId: 'social-network-laborator-8e757',
+  storageBucket: 'social-network-laborator-8e757.appspot.com',
+  messagingSenderId: '756336233738',
+  appId: '1:756336233738:web:264cbc604152705fdb3ef3',
+};
